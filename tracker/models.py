3689fd07d12c90a5da88c45c198d89aca88c4000
Henry Barcalow <henry.barcalow@gmail.com>
2025-11-18 18:23:31 -0500

    Dynamically update the view logs page

--- tracker/models.py
+++ tracker/models.py
@@ -7,10 +7,10 @@ class Log(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
-    song = models.CharField(default="No song", help_text="Name of song practiced.")
+    song = models.CharField(default="General practice", help_text="Name of song practiced.")
     instrument = models.CharField(help_text="Name of instrument practiced.")
     practice_time = models.DurationField(default="00:00:00", help_text="Time practiced, in hours-minutes-seconds.")
     practice_date = models.DateField(default=timezone.now, help_text="Date practiced.")
 
     def __str__(self):
-        return f"{self.song} on {self.instrument} {self.practice_time}"
+        return f"{self.song} on {self.instrument} for {self.practice_time}"
